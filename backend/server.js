57864c8b39b3593b53ad3d1913bbad1db9e39b6a
Alex Markowitsch <alexmarkowitsch@googlemail.com>
2021-10-07 22:32:34 +0200

    updated nodejs configs

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
 require("dotenv").config();
+
+const express = require('express');
 const app = express();
-app.use(express.urlencoded({ extended: false }));
 
+const port = process.env.NODE_DOCKER_PORT || 8080;
 
-// set port, listen for requests
-const PORT = process.env.NODE_DOCKER_PORT || 8080;
-
-// load schemes
-const TemperatureItem = require('./models/Temperature');
-
-app.get('/', (req, res) => {
-
+/**
+ * Starting the Server
+ */
+app.listen(port, () => {
+    console.log("Server started successfully at http://localhost:${port}");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+/**
+ * Defining Routes
+ */
+app.get('/', function(req, res){
+    res.send('Hello World');
 });
+
+app.use(express.urlencoded({
+    extended: true
+}));
