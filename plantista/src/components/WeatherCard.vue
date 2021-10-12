f66465a5b87654b7ed6512c35441bdfcf94c6334
Jan Wieczorek <jan@MacBook-Air-von-Jan.local>
2021-10-12 10:58:27 +0200

    updates weathercard

--- plantista/src/components/WeatherCard.vue
+++ plantista/src/components/WeatherCard.vue
@@ -5,17 +5,20 @@
     outlined
   >
     <v-list-item three-line>
-      <v-list-item-content>
-        <div class="text-overline mb-4">
-          Wetter
-        </div>
-        <div v-if="dataDings" class="weather">
-            {{dataDings.weather}}
-        </div>
+        <v-list-item-content>
+            <div
+                v-if="weatherData"
+                class="text-overline mb-4"
+            >
+                Aktuelles Wetter in {{weatherData.name}} 
+                <v-list-item> {{weatherData.weather[0].description}} </v-list-item>
+                <v-list-item> Temperatur: {{currentTemperate}}°C </v-list-item>
+                <v-list-item> Luftfeuchtigkeit: {{weatherData.main.humidity}}% </v-list-item>
+            </div>
 
-      </v-list-item-content>
+        </v-list-item-content>
     </v-list-item>
-  </v-card>
+</v-card>
 </template>
 
 <script>
@@ -24,20 +27,28 @@
     data()
     {
         return {
-            dataDings: null
+            weatherData: null
         };
     },
     methods: {
         async weather ()
         {
-            const cityID = "Berlin";
+            const cityID = "Erfurt";
             const key = process.env.VUE_APP_WEATHER_API_KEY;
 
             if (!key) { return; }
             
             const apiData = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + cityID + '&APPID=' + key)
             
-            this.dataDings  = await apiData.json();
+            this.weatherData  = await apiData.json();
+            console.log(this.weatherData);
+        },
+    },
+    computed: {
+        currentTemperate()
+        {
+            if (!this.weatherData) { return; }
+            return Math.round(this.weatherData.main.temp - 273.15);
         }
     },
     mounted()
@@ -46,10 +57,4 @@
     }
   }
   
-</script> 
-
-<style>
-
-
-
-</style>
+</script>
